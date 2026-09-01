b76b1c19f35c1463fc14b0ee09dcfd6eb02506b5
nishantchaurasiya22 <25f2008456@ds.study.iitm.ac.in>
2026-09-01 19:38:55 +0530

    update register ui

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,41 +1,33 @@
 import "../styles/form.scss"
 import { RiLockPasswordFill } from "react-icons/ri";
 import { IoIosContact } from "react-icons/io";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 import { Link } from "react-router";
-
 const Register = () => {
   return (
     <main>
-         <div className='auth-section'>
-         <h1 className='auth-title'>Register</h1>
-         <form className='auth-form-section'>
+      <div className='auth-section'>
+        <h1 className='auth-title'>Login</h1>
+        <form className='auth-form-section'>
           <div className="input-section">
-           <IoIosContact />
+            <IoIosContact />
             <input type="text" placeholder='username' />
           </div>
-          <div className="input-section">
-           <IoIosContact />
+           <div className="input-section">
+            <MdOutlineAlternateEmail />
             <input type="email" placeholder='Email' />
           </div>
           <div className="input-section">
-           <IoIosContact />
-            <input type="text" placeholder='bio' />
+            <RiLockPasswordFill />
+            <input type="password" placeholder='Password' />
           </div>
-          <div className="input-section">
-           <IoIosContact />
-            <input type="file" placeholder='profile image' />
-          </div>
-           <div className="input-section">
-             <RiLockPasswordFill />
-             <input type="password" placeholder='Password'/>
-           </div>
-           <button>Register</button>
-         </form>
-         <div className="auth-form-link">
-             <p>have an account? <Link className="link" to="/">Login</Link> </p>
-         </div>
-       </div>
-     </main>
+          <button>Login</button>
+        </form>
+        <div className="auth-form-link">
+          <p>have an account? <Link className="link" to="/">Login</Link> </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
